bd304198e79271f3c0f5365d06a68eaf319dcae8
Gregor <gregorlennie@hotmail.co.uk>
2020-04-19 15:55:02 +0100

    Adds details activity for bookmarks

---
+++ app/src/main/java/com/example/android/placebook/ui/BookMarkDetailsActivity.kt
@@ -0,0 +1,19 @@
+package com.example.android.placebook.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.example.android.placebook.R
+import kotlinx.android.synthetic.main.activity_bookmark_details.*
+
+class BookMarkDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmark_details)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar as Toolbar?)
+    }
+}
